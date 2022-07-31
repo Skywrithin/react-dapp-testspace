d9bc7ccf7d187c65cdf854222216ebb9fa144642
Kaleb Franken <skywrithin@gmail.com>
2022-07-31 17:01:23 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
+import { useState } from 'react';
+import { ethers } from 'hardhat';
+
+import Lock from '../src/artifacts/contracts/Lock.sol/Lock.json';
+
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  // const greeting =
+
+  const lockAddress = '0xc97c731c92eb1cb694f6143ac6433c2f35bbbfdde89f8d197a86b0a47778673e';
+  const [lock, setLock] = useState()
+
+async function fetchLock() {
+  if (!typeof window.ethereum !== 'undefined'){
+    const provider = new ethers.provider.Web3Provider(window.ethereum)
+    const contract = new ethers.Contract(lockAddress, Lock.abi, provider)
+    try {
+      // console.log(contract)
+      console.log('data:', data)
+    } catch (err) {
+      console.log("Error", err)
+    }
+  }
+}
+
+async function setLocktest() {
+
+} 
+
+async function requestAcc() {
+  await window.ethereum.request({ method: 'eth_requestAccounts' });
+}
+
+
+
   return (
     <div className="App">
       <header className="App-header">
